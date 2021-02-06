491fd86f72e68c8b7912effb83876ccfaaff7c30
ettoreMB <ettore.barbosa@gmail.com>
2021-02-06 20:03:26 -0300

    page links

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,24 @@ routes.get('/', (req,res) => {
   return res.render('pages/home')
 })
 
+routes.get('/eventos', (req,res) => {
+  return res.render('pages/eventos')
+})
+
+routes.get('/fotos', (req,res) => {
+  return res.render('pages/fotos')
+})
+
+routes.get('/videos', (req,res) => {
+  return res.render('pages/videos')
+})
+
+routes.get('/livros', (req,res) => {
+  return res.render('pages/livros')
+})
+
+routes.get('/contato', (req,res) => {
+  return res.render('pages/contato')
+})
+
 module.exports = routes
