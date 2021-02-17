9f10c3e6cb0e10ded8a17748d7f0915b628858b4
ettore <ettore.barbosa@gmail.com>
2021-02-17 20:19:45 -0300

    responive menu

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,17 @@
 function openNav() {
-  document.getElementById("sideNav").style.width = "250px"
+  document.getElementById("myTopnav").style.width = "90%"
 }
 
 function closeNav() {
-  document.getElementById("sideNav").style.width = "0"
+  document.getElementById("myTopnav").style.width = "0"
 }
+
+
+// function toogleMenu() {
+//   const x  = document.getElementById("myTopnav")
+//   if( x.className === "phone-menu") {
+//     x.className += " responsive"
+//   } else {
+//     x.className = "phone-menu"
+//   }
+// }
